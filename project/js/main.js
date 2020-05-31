630df7bf7a362685aecf060091611cf47d4df367
Cooper Elias <cooper.elias@mdoholdings.com>
2020-05-31 12:56:41 -0400

    Updated API endpoints

--- project/js/main.js
+++ project/js/main.js
@@ -2,34 +2,36 @@
 //  getUser();
 // })
 
-function getUser(searchQuery) {
-  var url = "https://www.clubready.com/api/users/{UserId}?UserId="+searchQuery+"&ChainId=18&FullDetail=true&ApiKey="+ApiKey;
+function userSearch() {
+  var searchQuery = $(".search").val();
+  findUser(searchQuery);
+}
 
-  $(".city").text("");
-  $(".dob").text("");
+function findUser(searchQuery) {
+  var url = "https://www.clubready.com/api/users/find?ApiKey="+ApiKey+"&ChainId=18&Barcode="+searchQuery;
+
   $(".error-message").text("");
 
   $.ajax(url,{success: function(data){
+    var userId = data.users[0].UserId;
+    console.log(userId);
+    getUser(userId);
+  }, error: function(error){
+    $(".error-message").text("Barcode not found");
+  }})
+}
+
+function getUser(userId) {
+  var url = "https://www.clubready.com/api/users/{UserId}?ApiKey="+ApiKey+"&ChainId=18&FullDetail=true&UserId="+userId;
+
+  $(".city").text("");
+  $(".dob").text("");
+
+  $.ajax(url,{success: function(data){
     console.log(data);
-    $(".city").text(data.City);
+    $(".city").text(data.FirstName + " " + data.LastName);
     $(".dob").text(data.DateOfBirth);
   }, error: function(error){
     $(".error-message").text("User not found");
   }})
-}
-
-function findUser() {
-  var searchQuery = $(".search").val();
-  getUser(searchQuery);
-}
-
-
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
-
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
 }
