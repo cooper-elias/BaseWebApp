0247401f82e727d7de31da4657217d2ed0d371c9
Cooper Elias <cooper.elias@mdoholdings.com>
2020-05-31 11:52:07 -0400

    Added CR API integration

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,29 @@
+// $(document).ready(function(){
+//  getUser();
+// })
+
+function getUser(searchQuery) {
+  var url = "https://www.clubready.com/api/users/{UserId}?UserId="+searchQuery+"&ChainId=18&FullDetail=true&ApiKey="+ApiKey;
+
+  $(".city").text("");
+  $(".dob").text("");
+  $(".error-message").text("");
+
+  $.ajax(url,{success: function(data){
+    console.log(data);
+    $(".city").text(data.City);
+    $(".dob").text(data.DateOfBirth);
+  }, error: function(error){
+    $(".error-message").text("User not found");
+  }})
+}
+
+function findUser() {
+  var searchQuery = $(".search").val();
+  getUser(searchQuery);
+}
+
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
